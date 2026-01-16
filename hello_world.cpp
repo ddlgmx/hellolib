c088629797b0132222051919fd176660d405ca27
ddlgmx <brianalmendoza@naver.com>
2026-01-15 22:18:54 -0500

    Initital commit of hellolib

---
+++ hello_world.cpp
@@ -0,0 +1,6 @@
+#include "hello_world.h"
+#include <iostream>
+
+void HelloWorld::print() {
+    std::cout << "Hello, World! From the library!\n" << std::endl;
+}
---
+++ include/hello_world.h
@@ -0,0 +1,10 @@
+#ifndef HELLO_WORLD_H
+#define HELLO_WORLD_H
+
+class HelloWorld {
+public: 
+    void print();
+    // this is the the menu item for hello world
+    // it will print hello world to the console
+};
+#endif
